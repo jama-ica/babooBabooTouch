561d2139d325a879913ad4eb4c6db67cb92a9f28
Igarashi <iga.hiroyuki@gmail.com>
2018-05-04 15:46:27 +0900

    ボタンを押して色が変わるところまで

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		GameObject obj = getClickObject();
+		if (obj != null)
+		{
+			float changeRed = 0.0f;
+			float changeGreen = 0.0f;
+			float cahngeBlue = 1.0f;
+			float cahngeAlpha = 1.0f;
+			// 元の画像がそのまま表示される。
+			this.GetComponent<SpriteRenderer>().color = new Color(changeRed, changeGreen, cahngeBlue, cahngeAlpha);
+		}
+	}
+
+	// 左クリックしたオブジェクトを取得する関数(2D)
+	private GameObject getClickObject()
+	{
+		GameObject result = null;
+		// 左クリックされた場所のオブジェクトを取得
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
+			if (collition2d)
+			{
+				result = collition2d.transform.gameObject;
+			}
+		}
+		return result;
+	}
+}
